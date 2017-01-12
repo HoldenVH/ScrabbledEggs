fddeb3bab2157a378fed7d31d6e2ebac31fbe7ac
henryz2000 <hzheng3@stuy.edu>
2017-01-12 10:14:24 -0500

    worked on row numbers

--- game/Board.java
+++ game/Board.java
@@ -1,5 +1,5 @@
 public class Board{
-    public static String[][] board = new String[31][31];
+    public static String[][] board = new String[32][32];
     public String toString(){
 	String retStr = "";
 	for( int i = 0; i < board.length; i++ ) {
@@ -18,23 +18,59 @@ public class Board{
 	}
     }
     public static void populate(String args[][]){
-	for (int x = 1; x < args.length; x+=2){
-	    for (int y = 1; y < args[x].length; y+=2){
+	//==============================
+	//creates top row column numbers
+	//==============================
+	
+	//sets column numbers at top
+	for (int y = 2; y < args[0].length; y+=2){
+	    args[0][y] = (y/2)-1 + "";
+	}
+	//sets spaces between single digit column numbers
+	for (int y = 1; y < 20; y+=2){
+	    args[0][y] = " ";
+	}
+	//defines placeholder values for double digit column numbers
+	for (int y = 21; y < args[0].length; y+=2){
+	    args[0][y] = "";
+	}
+
+	//==============================
+	//creates side row numbers
+	//==============================
+
+	//sets row numbers at left
+       	for (int x = 2; x < args[0].length; x+=2){
+	    args[x][0] = (x/2)-1 + "";
+	}
+
+	//for (
+
+	//==============================
+       	//creates board
+	//==============================
+	
+	//sets letter values
+	for (int x = 2; x < args.length; x+=2){
+	    for (int y = 2; y < args[x].length; y+=2){
 		args[x][y] = "a";
 	    }
 	}
-	for (int x = 1; x < args.length; x+=2){
-	    for (int y = 0; y < args[x].length; y+=2){
+	//creates vertical column borders
+	for (int x = 2; x < args.length; x+=2){
+	    for (int y = 1; y < args[x].length; y+=2){
 		args[x][y] = "|";
 	    }
 	}
-	for (int x = 0; x < args.length; x+=2){
-	    for (int y = 1; y < args[x].length; y+=2){
+	//creates horizontal row borders
+	for (int x = 1; x < args.length; x+=2){
+	    for (int y = 2; y < args[x].length; y+=2){
 		args[x][y] = "-";
 	    }
 	}
-	for (int x = 0; x < args.length; x+=2){
-	    for (int y = 0; y < args[x].length; y+=2){
+	//sets spaces between horizontal row borders
+	for (int x = 1; x < args.length; x+=2){
+	    for (int y = 1; y < args[x].length; y+=2){
 		args[x][y] = " ";
 	    }
 	}
