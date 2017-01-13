f0308647f842dd7aac1856fe773f889153d4e486
DavidApterman <dapterman@stuy.edu>
2017-01-12 20:24:20 -0500

    added rack letters

--- game/User.java
+++ game/User.java
@@ -5,7 +5,10 @@ public class User extends Player{
     //INPUT FORMAT: <x1> <y1> <dir(d/r)> <word>
     public User(){
 	score=0;
-	rack=new ArrayList<Character>();
+	rack=new ArrayList<Character>{'A','A','A','A','A','A','A','A','A','B','B','C','C','D','D','D','D','E','E','E','E','E','E','E','E','E','E','E','E','F','F','G','G','G','H','H',
+		'I','I','I','I','I','I','I','I','I','J','K','L','L','L','L','M','M','N','N','N','N','N','N','O','O','O','O','O','O','O','O','Q','R','R','R','R','R','R','S','S','S','S',
+		'T','T','T','T','T','T','U','U','U','U','V','V','W','W','X','Y','Y','Z' 
+		};
     }
     public User(String n){
 	this();
