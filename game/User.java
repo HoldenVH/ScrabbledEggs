e522a9965c47c3bcab6737fafa42ad90e45521f7
DavidApterman <dapterman@stuy.edu>
2017-01-18 22:55:19 -0500

    Update User.java

--- game/User.java
+++ game/User.java
@@ -22,15 +22,21 @@ public class User extends Player{
 	input.add(Keyboard.readWord());//<word>
 	int x = (int)input.get(0) - 1;
 	int y = (int)input.get(1) - 1;
+	String dirstr = (String)input.get(2);
 	String inputstr = (String)input.get(3);
-	if(Board.emptyCheck() && Board.isWord(inputstr) ){
-	    input.add(1);
+	//check if entry is within bounds
+	if( (dirstr.equals("r")&&(x+inputstr.length() > 16) )
+	    ||
+	    (dirstr.equals("d")&&(y+inputstr.length() > 16) ) ){
+	    input.add(2);//signifies out of bounds
 	}
+	//now check if entry is a word
 	else if(Board.isWord(inputstr) ) { 
-	    input.add(1);
+	    input.add(1); //signifies word within bounds
 	}
+	//in bounds but not a word
 	else{
-	    input.add(0);
+	    input.add(0);//signifies non-word within bounds
 	}
 	return input;
     }
@@ -50,3 +56,4 @@ public class User extends Player{
 	    }
     }
 }
+
