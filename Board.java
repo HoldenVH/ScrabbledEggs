de5ac3e656c75ca0e34f3667dfe0c70e2c0954e0
DavidApterman <dapterman@stuy.edu>
2017-01-22 14:55:08 -0500

    restored Bag to its former glory

--- Board.java
+++ Board.java
@@ -8,8 +8,8 @@ public class Board{
     public static String[][] displayBoard = new String[32][32];
     private static char[][] board=new char[15][15];//actual backend board
     private static char[][] boardTemp = new char[15][15]; //temp backend board
-    public static final char[] STARTING_BAG={'G','A','M','B','I','T','S','A','T'};
-    //   public static final char[] STARTING_BAG={'A','A','A','A','A','A','A','A','A','B','B','C','C','D','D','D','D','E','E','E','E','E','E','E','E','E','E','E','E','F','F','G','G','G','H','H','I','I','I','I','I','I','I','I','I','J','K','L','L','L','L','M','M','N','N','N','N','N','N','O','O','O','O','O','O','O','O','Q','R','R','R','R','R','R','S','S','S','S','T','T','T','T','T','T','U','U','U','U','V','V','W','W','X','Y','Y','Z'};
+    //  public static final char[] STARTING_BAG={'G','A','M','B','I','T','S','A','T'};
+    public static final char[] STARTING_BAG={'A','A','A','A','A','A','A','A','A','B','B','C','C','D','D','D','D','E','E','E','E','E','E','E','E','E','E','E','E','F','F','G','G','G','H','H','I','I','I','I','I','I','I','I','I','J','K','L','L','L','L','M','M','N','N','N','N','N','N','O','O','O','O','O','O','O','O','Q','R','R','R','R','R','R','S','S','S','S','T','T','T','T','T','T','U','U','U','U','V','V','W','W','X','Y','Y','Z'};
     public static ArrayList<Character> BAG = new ArrayList<Character>(STARTING_BAG.length);
     private static int[] score = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};//score of each letter in scrabble
     private static int wordScore;
